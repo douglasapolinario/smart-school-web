588c0d61630c25ca29cdac6497ed19b033edc580
Douglas Apolinario <doliveira.apolinario@gmail.com>
2017-12-21 23:08:16 -0200

    cria form reativo

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      usuario: [null, Validators.required],
+      senha: [null, Validators.required]
+    });
+  }
+
+  login(): void {
+    console.log(this.formulario);
   }
 
 }
